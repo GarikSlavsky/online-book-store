f76eac3616c1d94b40fb2a10262f234720fec367
Igor Neminuschii <igorgorslav@gmail.com>
2025-01-27 19:57:03 +0200

    removed incorrect annotation

--- src/main/java/mate/academy/onlinebookstore/repository/SpecificationProviderManager.java
+++ src/main/java/mate/academy/onlinebookstore/repository/SpecificationProviderManager.java
@@ -1,8 +1,5 @@
 package mate.academy.onlinebookstore.repository;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public interface SpecificationProviderManager<T> {
     SpecificationProvider<T> getSpecificationProvider(String determiner);
 }
