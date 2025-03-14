0fba95b2c635334bee07d72a21d93bf0c45f52c6
Igor Neminuschii <131386977+GarikSlavsky@users.noreply.github.com>
2025-03-14 22:06:39 +0200

    docker (#14)
    * added pagination, sorting and Swagger
    
    * relocated the springdoc version
    
    * Added User entity
    
    * improved the register method within the UserServiceImpl class
    
    * Delete src/main/resources/db/changelog/changes/04-rename-fields.yaml
    
    * fixed the name of the file 03-create-users-table.yaml
    
    * intermediate commit
    
    * intermediate commit #2
    
    * added User entity
    
    * Delete src/main/resources/db/changelog/changes/07-insert-users.yaml
    
    * added changeSet 02
    
    * the ability to set roles via HTTP requests has been removed
    
    * implemented JWT support
    
    * added mockito dependency
    
    * app.prop files
    
    * added a constant into the JwtAuthenticationFilter class
    
    * added the Category entity adn related functionality
    
    * added annotation @NotEmpty to the categoryIds field
    
    * fixed  lexicographical order for import statements
    
    * added cartController
    
    * implemented cartController
    
    * added user ID to the cartController methods
    
    * added a method in order to avoid extra SELECT queries
    
    * reorganized the CartServiceImpl class
    
    * deleted unused import
    
    * added Order and OrderItem entities and related services
    
    * added authentication to the controller methods
    
    * added Docker
    
    * removed .env file from the version control

--- src/main/java/mate/academy/onlinebookstore/repository/order/OrderRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/order/OrderRepository.java
@@ -1,15 +1,15 @@
 package mate.academy.onlinebookstore.repository.order;
 
-import java.util.List;
 import java.util.Optional;
 import mate.academy.onlinebookstore.model.Order;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @EntityGraph(attributePaths = "orderItems")
-    List<Order> findAllByUserId(Long userId, Pageable pageable);
+    Page<Order> findAllByUserId(Long userId, Pageable pageable);
 
     @EntityGraph(attributePaths = "orderItems")
     Optional<Order> findByIdAndUserId(Long id, Long userId);
