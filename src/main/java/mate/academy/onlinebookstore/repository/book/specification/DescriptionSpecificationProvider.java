0d60da5c0aca72216a4d6f0fc2732c8d420c9c43
Igor Neminuschii <131386977+GarikSlavsky@users.noreply.github.com>
2025-04-01 10:47:26 +0300

    readme (#22)
    * added pagination, sorting and Swagger
    
    * relocated the springdoc version
    
    * Added User entity
    
    * improved the register method within the UserServiceImpl class
    
    * Delete src/main/resources/db/changelog/changes/04-rename-fields.yaml
    
    * fixed the name of the file 03-create-users-table.yaml
    
    * intermediate commit
    
    * intermediate commit #2
    
    * added User entity
    
    * Delete src/main/resources/db/changelog/changes/07-insert-users.yaml
    
    * added changeSet 02
    
    * the ability to set roles via HTTP requests has been removed
    
    * implemented JWT support
    
    * added mockito dependency
    
    * app.prop files
    
    * added a constant into the JwtAuthenticationFilter class
    
    * added the Category entity adn related functionality
    
    * added annotation @NotEmpty to the categoryIds field
    
    * fixed  lexicographical order for import statements
    
    * added cartController
    
    * implemented cartController
    
    * added user ID to the cartController methods
    
    * added a method in order to avoid extra SELECT queries
    
    * reorganized the CartServiceImpl class
    
    * deleted unused import
    
    * added Order and OrderItem entities and related services
    
    * added authentication to the controller methods
    
    * added Docker
    
    * removed .env file from the version control
    
    * added test cases for Book and Category entities
    
    * added invalid test cases
    
    * added verify() for all mocked methods
    
    * moved test private methods to util classes
    
    * moved test private methods to util classes
    
    * improved assertion in order to not use a method that does not consider the sequence of objects
    
    * added test cases for ShoppingCart entity
    
    * Update2 README.md
    
    * added diagrams
    
    * fixed user register method
    
    * completed README file
    
    * added the description of application controllers

--- src/main/java/mate/academy/onlinebookstore/repository/book/specification/DescriptionSpecificationProvider.java
+++ src/main/java/mate/academy/onlinebookstore/repository/book/specification/DescriptionSpecificationProvider.java
@@ -2,6 +2,7 @@ package mate.academy.onlinebookstore.repository.book.specification;
 
 import static mate.academy.onlinebookstore.dto.book.BookSearchParametersDto.DESCRIPTION;
 
+import jakarta.persistence.criteria.JoinType;
 import mate.academy.onlinebookstore.model.Book;
 import mate.academy.onlinebookstore.repository.SpecificationProvider;
 import org.springframework.data.jpa.domain.Specification;
@@ -17,8 +18,11 @@ public class DescriptionSpecificationProvider implements SpecificationProvider<B
     @Override
     public Specification<Book> getSpecification(Object... params) {
         String description = (String) params[0];
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(
-                root.get(DESCRIPTION), "%" + description + "%"
-        );
+        return (root, query, criteriaBuilder) -> {
+            root.fetch("categories", JoinType.LEFT);
+            return criteriaBuilder.like(
+                    root.get(DESCRIPTION), "%" + description + "%"
+            );
+        };
     }
 }
--- src/main/java/mate/academy/onlinebookstore/repository/book/specification/TitleSpecificationProvider.java
+++ src/main/java/mate/academy/onlinebookstore/repository/book/specification/TitleSpecificationProvider.java
@@ -2,6 +2,7 @@ package mate.academy.onlinebookstore.repository.book.specification;
 
 import static mate.academy.onlinebookstore.dto.book.BookSearchParametersDto.TITLE;
 
+import jakarta.persistence.criteria.JoinType;
 import mate.academy.onlinebookstore.model.Book;
 import mate.academy.onlinebookstore.repository.SpecificationProvider;
 import org.springframework.data.jpa.domain.Specification;
@@ -16,8 +17,10 @@ public class TitleSpecificationProvider implements SpecificationProvider<Book> {
 
     @Override
     public Specification<Book> getSpecification(Object... params) {
-        return (root, query, criteriaBuilder) -> root
-                .get(TITLE)
-                .in(params);
+        return (root, query, criteriaBuilder) -> {
+            root.fetch("categories", JoinType.LEFT);
+            return root.get(TITLE)
+                    .in(params);
+        };
     }
 }
